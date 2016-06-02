4d2991c4dd525318f8a348451556af1b7364402b
Nick Hibberd <nhibberd@gmail.com>
2016-06-02 17:27:49 +1000

    VOLT

--- boris-client/src/Boris/Client/Http.hs
+++ boris-client/src/Boris/Client/Http.hs
@@ -14,6 +14,7 @@ import           Data.Aeson (FromJSON (..), ToJSON (..), encode)
 import           Data.ByteString (ByteString)
 import qualified Data.ByteString.Lazy as BL
 import qualified Data.Text as T
+import qualified Data.Text.Encoding as T
 
 import qualified Network.HTTP.Client as H
 import           Network.HTTP.Types (Status (..))
@@ -21,7 +22,7 @@ import qualified Network.HTTP.Types as HTTP
 
 import           P
 
-import           Snooze.Balance.Control (BalanceConfig, BalanceError (..), runBalanceT)
+import           Snooze.Balance.Control (BalanceConfig (..), BalanceError (..), runBalanceT)
 import           Snooze.Balance.Http (httpBalanced)
 import           Snooze.Url (encodePathSegmentsBS)
 import           Snooze.Json (decodeResponse)
@@ -30,10 +31,15 @@ import           System.IO (IO)
 
 import           X.Control.Monad.Trans.Either (EitherT, newEitherT, bimapEitherT, hoistEither, left)
 
+import qualified Volt.Data.Config as Volt
+import qualified Volt.Data.Request as Volt
+import qualified Volt.Http.Raw as Volt
+
 data BorisHttpClientError =
     BorisHttpClientBalanceError BalanceError
   | BorisHttpClientDecodeError Text
   | BorisHttpClientUnhandledResponseError (H.Response BL.ByteString)
+  | BorisHttpClientVoltError Volt.VoltHttpError
 
 borisVersion :: ByteString
 borisVersion =
@@ -41,11 +47,19 @@ borisVersion =
 
 delete :: BalanceConfig -> [Text] -> EitherT BorisHttpClientError IO ()
 delete b url = do
+  res <- bimapEitherT BorisHttpClientVoltError id $
+    Volt.delete
+      (Volt.VoltConfig (balanceConfigHttpManager b) (remoteRetryPolicy b) Volt.BalanceTable)
+      (Volt.http "boris.ambiata.com" (fmap T.encodeUtf8 url))
+      mempty
+
+{-
   res <- bimapEitherT BorisHttpClientBalanceError id . newEitherT . runBalanceT b . httpBalanced $ \r -> r {
       H.path = encodePathSegmentsBS url
     , H.requestHeaders = [(HTTP.hContentType, borisVersion)]
     , H.method = HTTP.methodDelete
     }
+-}
   case H.responseStatus res of
     Status 202 _ ->
       pure ()
